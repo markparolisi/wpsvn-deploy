4fd4145caebe387f050f9587d34c504994b844f0
Mark Parolisi <mark@voceconnect.com>
2012-10-18 13:09:13 -0400

    formatting and checking to proceed if git tag exists

--- wpsvn-deploy.sh
+++ wpsvn-deploy.sh
@@ -3,23 +3,15 @@
 # A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
-USAGE="Usage: \n
--n = Plugin slug name ex. page-links-to. DEFAULT: use working directory(pwd) \n
--u = REQUIRED. WP SVN username \n
--f = Plugin mainfile. DEFAULT: {plugin slug}.php \n
--p = SVN repo path. DEFAULT: http://plugins.svn.wordpress.org/{plugin slug} \n
--d = Git directory. DEFAULT: use working directory(pwd).
-"
-while getopts ":n:f:p:u:d:h" OPTIONS; do
-  case $OPTIONS in
-    n ) PLUGINSLUG=$OPTARG;;
-    f ) MAINFILE=$OPTARG;;
-	p ) SVNURL=$OPTARG;;
-	u ) SVNUSER=$OPTARG;;
-	d ) CURRENTDIR=$OPTARG;;
-    h ) echo -e $USAGE; exit;
-  esac
-done
+USAGE="
+Usage: \n 
+-n = Plugin slug name ex. page-links-to. DEFAULT: use working  directory(pwd) \n 
+-u = REQUIRED. WP  SVN  username \n  
+-f = Plugin  mainfile.  DEFAULT: {plugin  slug}.php  \n  
+-p = SVN repo  path.  DEFAULT: http://plugins.svn.wordpress.org/{plugin slug}  \n 
+-d = Git  directory. DEFAULT:  use working  directory(pwd). "
+while getopts  ":n:f:p:u:d:h" OPTIONS; do case  $OPTIONS in n )  PLUGINSLUG=$OPTARG;; f ) MAINFILE=$OPTARG;;  p )
+SVNURL=$OPTARG;;  u  )  SVNUSER=$OPTARG;;  d  )  CURRENTDIR=$OPTARG;;  h  )  echo  -e  $USAGE;  exit;  esac  done
 
 # Set the variables based on flag input or defaults
 if [ -z "$PLUGINSLUG" ];then PLUGINSLUG=${PWD##*/}; fi
@@ -48,15 +40,29 @@ echo "$MAINFILE version: $NEWVERSION2"
 
 if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
 
+echo "Versions match in readme.txt and PHP file. Let's proceed..."
+
 #check if tag of that version already exists and exit script
 cd $GITPATH
 tag_exists=`git ls-remote --tags |  grep "tags/$NEWVERSION1" | grep -v grep | awk ' {print $2}'`
 if [ "$tag_exists" ]
     then
-      echo "Tag already exists."; exit;
+        promptyn () {
+            while true; do
+                read -p "$1 " yn
+                case $yn in
+                    [Yy]* ) return 0;;
+                    [Nn]* ) return 1;;
+                    * ) echo "(yes or no)";;
+                esac
+            done
+        }
+        if promptyn "Tag already exists. Do you wish to continue?"; then
+            echo "Continuing...";
+        else
+            echo "Exiting..."; exit 1; 
+        fi
 fi
-
-echo "Versions match in readme.txt and PHP file. Let's proceed..."
 
 cd $GITPATH
 echo -e "Enter a commit message for this new version: \c"
